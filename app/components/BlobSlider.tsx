f416e3e53b26d9f7d3b38538d9758a279244d7bd
Yusuf Yusuf <yusuf.1.yusuf@kcl.ac.uk>
2024-02-18 07:50:04 +0000

    Set up Remix/Chakra UI/BlobSlider

---
+++ app/components/BlobSlider.tsx
@@ -0,0 +1,105 @@
+import {
+  VStack,
+  Stack,
+  Slider,
+  Box,
+  SliderTrack,
+  Text,
+  SliderFilledTrack,
+  SliderThumb,
+  keyframes,
+} from "@chakra-ui/react";
+import { motion } from "framer-motion";
+import AIRealBlob from "~/images/AIRealBlob";
+import AIYusuf from "~/images/AIYusuf";
+import RealYusuf from "~/images/RealYusuf";
+import { useState, useEffect } from "react";
+
+export default function BlobSlider() {
+  // Used for controlling the split in the Real/AI photo
+  const [blobSliderValue, setBlobSliderValue] = useState(0);
+  const [animationFinished, setAnimationFinished] = useState(false);
+
+  const handleBlobSliderChange = (val: number) => {
+    setBlobSliderValue(val);
+  };
+
+  const RealYusufAnimationKeyframes = animationFinished
+    ? null
+    : keyframes`
+  0% { width: 100%; }
+  10% { width: 40%; }
+  40% { width: 60%; }
+  70% { width: 0%; }
+  100% { width: 100%; }
+`;
+
+  const RYanimation = `${RealYusufAnimationKeyframes} 8s ease-in-out `;
+
+  useEffect;
+
+  return (
+    <VStack>
+      <Stack
+        w="600px"
+        h="600px"
+        p={0}
+        m={0}
+        position="relative"
+        alignItems="end"
+        justifyContent="center"
+      >
+        <Box
+          w="full"
+          h="full"
+          zIndex={1}
+          position="absolute"
+          bottom={0}
+          right={0}
+        >
+          <AIRealBlob />
+        </Box>
+        <VStack
+          as={motion.div}
+          animation={RYanimation}
+          onAnimationEnd={() => setBlobSliderValue(100)}
+          w={`${blobSliderValue}%`}
+          position="absolute"
+          p={0}
+          m={0}
+          left={0}
+          h="94.2%"
+          alignItems="center"
+          justifyContent="center"
+          zIndex={3}
+          border="0"
+        >
+          <RealYusuf />
+        </VStack>
+        <VStack
+          w={`${100 - blobSliderValue}%`}
+          h="94.2%"
+          position="absolute"
+          right={0}
+          alignItems="center"
+          justifyContent="center"
+          zIndex={2}
+        >
+          <AIYusuf />
+        </VStack>
+      </Stack>
+      <Slider
+        aria-label="slider-ex-1"
+        defaultValue={0}
+        value={blobSliderValue}
+        onChange={(val) => handleBlobSliderChange(val)}
+      >
+        <SliderTrack>
+          <SliderFilledTrack />
+        </SliderTrack>
+        <SliderThumb />
+      </Slider>
+      <Text size="sm">👈 Cartoon | Me 👉</Text>
+    </VStack>
+  );
+}
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,33 @@
+import { Container, Flex, VStack } from "@chakra-ui/react";
 import type { MetaFunction } from "@remix-run/node";
+import "public/styles/homepage_styles.css";
+import BlobSlider from "~/components/BlobSlider";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Yusuf Yusuf Portfolio" },
+    {
+      name: "Find out about my work so far.",
+      content: "Welcome to my portfolio!",
+    },
   ];
 };
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <Container maxWidth="container.xl" p={0}>
+      <Flex h="100vh" py={20}>
+        <VStack
+          w="full"
+          h="full"
+          p={10}
+          spacing={10}
+          alignItems="flex-start"
+        ></VStack>
+        <VStack bg="gray.50" w="full" h="full">
+          <BlobSlider />
+        </VStack>
+      </Flex>
+    </Container>
   );
 }
